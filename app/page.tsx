9d2e0438690192780cac68c1e887e94794dd73f7
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2023-05-18 23:58:00 -0500

    fix css

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <main className="flex items-start min-h-screen font-sans flex-col p-24">
-        <div className="w-full max-w-1xl md:flex">Hey! My name is Trevor.</div>
-        <div className="w-full max-w-2xl justify-center md:flex">
+      <main className="flex w-full items-start min-h-screen font-mono flex-col p-24">
+        <div className="w-full max-w-8xl flex">Hey! My name is Trevor.</div>
+        <div className="w-full max-w-8xl justify-center flex">
           Welcome to my site.
         </div>
-        <div className="w-full max-w-5xl justify-end md:flex">
+        <div className="w-full max-w-8xl justify-end flex">
           It is a little messy, but I like it this way.
         </div>
-        <div className="w-full max-w-5xl mt-12 justify-center md:flex">
+        <div className="w-full max-w-8xl mt-12 justify-center flex">
           {
             "I'm working on a few things and will post them here when they are done."
           }
         </div>
-        <div className="w-full max-w-5xl mt-12 justify-between md:flex">
+        <div className="w-full mt-12 justify-between flex">
           <a href="https://github.com/tbrixey" target="_blank">
             <button className="rounded-full bg-[#4078c0] px-8 py-2">
               GitHub
